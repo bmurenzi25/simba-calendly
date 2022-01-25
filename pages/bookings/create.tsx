f44ceb90b9b4b1f45ad8341d6af9b8daacc8e672
Bienvenu Murenzi <bienvenu@paytrippa.com>
2022-01-25 23:23:57 +0200

    Feat, create Booking

--- pages/bookings/create.tsx
+++ pages/bookings/create.tsx
@@ -1,11 +1,64 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import axios from 'axios';
+import Router from 'next/router';
+
+interface FormBoookingValues {
+  username: string;
+  length: string;
+  time: string;
+  date: string;
+}
+
+const schema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  length: Yup.string().required('length field cannot be empty'),
+  time: Yup.string().required('time is a required field'),
+  date: Yup.string().required('Date is a required field'),
+});
 
 const Create = () => {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      Router.push('/');
+    }
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState('');
+
+  const {
+    createBooking,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormBoookingValues>({
+    resolver: yupResolver(schema),
+  });
+  const onSumbit: SubmitHandler<FormBoookingValues> = async (data) {
+    setIsLoading(true);
+    try {
+      const token = localStorage.getItem('token');
+      const res = await axios.post('/api/bookings/create', {
+        ...data,
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setIsLoading(false);
+    } catch (err: any) {
+      setError(err.message);
+      setIsError(true);
+      setIsLoading(false);
+    }
+  }
   const [value, onChange] = useState(new Date());
 
   return (
